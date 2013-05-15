ce8191cbe90c2b117913fb82d064af8e7af65186
Ben van Klinken <ustramooner@users.sourceforge.net>
2013-05-16 09:20:17 +1000

    new memcache version

--- support/set-env.sh
+++ support/set-env.sh
@@ -8,7 +8,7 @@ export APC_VERSION="3.1.10"
 export PHPREDIS_VERSION="2.2.1"
 export LIBMEMCACHED_VERSION="1.0.7"
 export LIFREETYPE_VERSION="2.4.12"
-export MEMCACHED_VERSION="2.2.7"
+export MEMCACHED_VERSION="2.1.0"
 export NEWRELIC_VERSION="3.4.5.167"
 
 export NGINX_VERSION="1.4.1"
@@ -16,3 +16,4 @@ export NGINX_VERSION="1.4.1"
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
 export EC2_CERT=~/.ec2/cert.pem
 export EC2_URL=https://ec2.us-east-1.amazonaws.com
+
